989e224d398c6bdeab0a6e20d87b957079d0439f
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-04-14 20:03:32 +0000

    [Quotient Rule Scene] Improvements to the practical example scene with expansion on definition of \frac{a^m}{a^n} as reminder

--- math/algebra/exponents.py
+++ math/algebra/exponents.py
@@ -20,7 +20,7 @@ green_gradient = [
 
 exponent_laws = {
     "product_rule": MathTex(r"a^m", r"\cdot", r"a^n", "=", r"a^{m+n}"),
-    "quotient_rule": MathTex(r"\frac{a^m}{a^n}", "=", r"a^{m-n}"),
+    "quotient_rule": MathTex(r"\frac" r"{a^m}{a^n}", "=", r"a^{m-n}"),
     "power_rule" :MathTex(r"(a", r"^m)", "=", r"a^{m+n}"),
     "power_of_quotient_rule" : MathTex(
             r"\left(",
@@ -235,18 +235,32 @@ class Quotient_Rule_Scene(Scene):
 
     def set_blue(self, thing:Mobject):
         thing.set_color_by_gradient(darker_blues)
+
+    def set_green(self, thing:Mobject):
+        thing.set_color_by_gradient(green_gradient)
     
     def practical_quotient_expansion(self):
-        elaborating = MathTex(r"\frac{x^m}{x^n}=\frac{\overbrace{x\cdot x \cdots}^m}{\underbrace{x\cdot x \cdots}_n}").set_color_by_gradient(darker_blues)
-        practical_example = MathTex(r"\frac{x^4}{x^2}=\frac{x" r"\cdot x" r"\cdot x " r"\cdot x" r"}" r"{x \cdot x}").set_color_by_gradient(darker_blues)
+        practical_example = MathTex(r"\frac" r"{x^4}" r"{x^2}" r"=\frac{" r"x" r"\cdot x" r"\cdot x " r"\cdot x" r"}" r"{x \cdot x}").set_color_by_gradient(darker_blues)
+        expansion = (
+            MathTex(r"=\frac{\overbrace{a\cdot a \cdots}^m}{\underbrace{a\cdot a \cdots}_n}")
+            .next_to(self.laws_group[1][0], RIGHT)    
+        )
+        self.set_green(expansion)
         # this a version of practical example
         bracket_version = MathTex(r"\frac{2^4}{2^2}=\frac{2" r"\cdot 2" r"\cdot 2 " r"\cdot 2" r"}" r"{2 \cdot 2}")
-        self.set_blue(bracket_version)
+        
         self.play(self.laws_group[1].animate.center())
-        #removing the quotient rule in favour of some practical example
-        self.play(ReplacementTransform(self.laws_group[1],elaborating))
-        self.wait(0.5)
-        self.play(FadeOut(elaborating), Write(practical_example))
+        # coloring the definition prt of the equaiton to emphasise it
+        self.play(self.laws_group[1][0].animate.set_color(green_gradient))
+        # fading out the rest of the equaiton to make room for its expansion
+        self.play(FadeOut(self.laws_group[1][1], self.laws_group[1][2]))
+        self.play(Write(expansion))
+        # self.play(ReplacementTransform(self.laws_group[1],elaborating))
+        # self.wait(0.5)
+
+        # self.play(FadeOut(elaborating))
+        # self.wait(0.5) 
+        # self.play(Write(practical_example))
         
 
         
